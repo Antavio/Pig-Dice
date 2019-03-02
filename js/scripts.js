bbed519cf7d50a579e0380793f6ac59cf276d3b1
Antavio <njuguna13@gmail.com>
2019-03-02 12:51:28 +0300

    create logic to stop player one from continuing to play if they roll number 1.

--- js/scripts.js
+++ js/scripts.js
@@ -6,45 +6,48 @@ function disablePlayerOne(){
 function disablePlayerTwo(){
   document.getElementById("roll-two").disabled=false;
   document.getElementById("roll-one").disabled=true;
-
 }
 
+var name1 = "";
+var name2 = "";
+var generatedNumber1 = 0;
+var generatedNumber2 = 0;
+var rollSum1 = 0;
+var rollSum2 = 0;
+var total1=0;
+var total2=0;
 $(document).ready(function(){
-  var randomScore1 = 0;
-  var randomScore2 = 0;
-  var rollScore1 = "";
-  var rollScore2 = "";
-  var sum1=0;
-  var sum2=0;
 
 
-  function Player(name,randomScore,rollScore,sum){
+  function Player(name,generatedNumber,rollSum,total){
     this.name=name;
-    this.randomScore=randomScore;
-    this.rollScore = rollScore;
-    this.sum = sum;
+    this.generatedNumber=generatedNumber;
+    this.rollSum = rollSum;
+    this.total = total;
   };
 
 
 
-  var playerOne = new Player(name,randomScore1,rollScore1,sum1);
-  var playerTwo = new Player(name,randomScore2,rollScore2,sum2);
+  var playerOne = new Player(name1,generatedNumber1,rollSum1,total1);
+  var playerTwo = new Player(name2,generatedNumber2,rollSum2,total2);
 
+  //Player One Logic
   $("#roll-one").click(function(){
-    randomScore1 = Math.floor(Math.random()*6 + 1);
-     $("#die-side").text(randomScore1);
+    generatedNumber1 = Math.floor(Math.random()*6 + 1);
+    $("#die-side").text(generatedNumber1);
     // alert(stored_random);
-    if(randomScore1!=1){
-      sum1+=randomScore1;
-      $("#current-score1").text(sum1);
+    if(generatedNumber1!=1){
+      total1+=generatedNumber1;
+      $("#current-score1").text(total1);
     } else {
       sum1=0;
-        $("#current-score1").text(sum1);
-        disablePlayerOne();
+      alert("Your die rolled on side 1 "+ "\n Next player to roll the die");
+      $("#current-score1").text(total1);
+      disablePlayerOne();
     }
   });
   $("#hold-player-one").click(function(){
-    alert("Your Score is: "+ sum1 + " \n Player Two turn to play");
+    alert("Your Score is: "+ total1 + " \n Player Two turn to play");
     disablePlayerOne();
   });
 });
