294e910b28780274d6af32c8b605b9bcb7ffd172
Antavio <njuguna13@gmail.com>
2019-03-02 13:07:15 +0300

    create a logic to score current total score of player one.

--- js/scripts.js
+++ js/scripts.js
@@ -16,9 +16,32 @@ var rollSum1 = 0;
 var rollSum2 = 0;
 var total1=0;
 var total2=0;
+
+//Player One Logic
+function playerOneLogic(){
+$("#roll-one").click(function(){
+  generatedNumber1 = Math.floor(Math.random()*6 + 1);
+  $("#die-side").text(generatedNumber1);
+  // alert(stored_random);
+  if(generatedNumber1!=1){
+    rollSum1+=generatedNumber1;
+    total1=rollSum1+generatedNumber1;
+    $("#current-score1").text(rollSum1);
+    $("#total-score1").text(total1);
+  } else {
+    rollSum1=0;
+    alert("Your die rolled on side 1 "+ "\n Next player to roll the die");
+    $("#current-score1").text(rollSum1);
+    disablePlayerOne();
+  }
+});
+$("#hold-player-one").click(function(){
+  alert("Your Total Score is: "+ total1 + " \n Player Two turn to play");
+  disablePlayerOne();
+});
+};
+
 $(document).ready(function(){
-
-
   function Player(name,generatedNumber,rollSum,total){
     this.name=name;
     this.generatedNumber=generatedNumber;
@@ -26,28 +49,8 @@ $(document).ready(function(){
     this.total = total;
   };
 
-
-
   var playerOne = new Player(name1,generatedNumber1,rollSum1,total1);
   var playerTwo = new Player(name2,generatedNumber2,rollSum2,total2);
+  playerOneLogic();
 
-  //Player One Logic
-  $("#roll-one").click(function(){
-    generatedNumber1 = Math.floor(Math.random()*6 + 1);
-    $("#die-side").text(generatedNumber1);
-    // alert(stored_random);
-    if(generatedNumber1!=1){
-      total1+=generatedNumber1;
-      $("#current-score1").text(total1);
-    } else {
-      sum1=0;
-      alert("Your die rolled on side 1 "+ "\n Next player to roll the die");
-      $("#current-score1").text(total1);
-      disablePlayerOne();
-    }
-  });
-  $("#hold-player-one").click(function(){
-    alert("Your Score is: "+ total1 + " \n Player Two turn to play");
-    disablePlayerOne();
-  });
 });
